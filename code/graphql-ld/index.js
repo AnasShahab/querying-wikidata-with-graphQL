4fe5b797161d3388fcd0e019614000547a387742
Shahab Anas <ashahab@nlberlin.deg.local>
2022-07-28 01:09:57 +0200

    Added a test code for grapghql-ld

---
+++ code/graphql-ld/index.js
@@ -0,0 +1,23 @@
+import {Client} from "graphql-ld";
+import {QueryEngineSparqlEndpoint} from "graphql-ld-sparqlendpoint";
+
+// Define a JSON-LD context
+const context = {
+  "@context": {
+    "label": { "@id": "http://www.w3.org/2000/01/rdf-schema#label" }
+  }
+};
+
+// Create a GraphQL-LD client based on a SPARQL endpoint
+const endpoint = 'http://dbpedia.org/sparql';
+const client = new Client({ context, queryEngine: new QueryEngineSparqlEndpoint(endpoint) });
+
+// Define a query
+const query = `
+  query @single {
+    label
+  }`;
+
+// Execute the query
+const { data } = await client.query({ query });
+console.log(data)
